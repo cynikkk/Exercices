e2bc18a5d5d2afc63ba8644904755d130b171b84
cynikkk <theo.abel53@gmail.com>
2020-11-15 16:34:35 +0100

    Algorithme basique du Pendu

---
+++ C++/pendu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    // Générer mot caché
+    // Initialiser vie (12 essais)
+    // Si lettre se trouve plusieurs fois afficher toutes les occurences
+    // Si lettre dans mot alors afficher lettre sinon vie--
+    // Afficher mot 
+    // Rendre lettres insensibles à la casse
+
+    return 0;
+}
