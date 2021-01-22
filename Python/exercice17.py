268fc99f4b4096bf6e39d276741ea8235b080d0d
cynikkk <theo.abel53@gmail.com>
2021-01-22 19:03:53 +0100

    J'en ai marre

--- Python/exercice17.py
+++ Python/exercice17.py
@@ -1,14 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import lxml
 
 # Getting HTML from URL in a string
-url = 'https://www.nytimes.com/'
-request = requests.get(url)
-requestHTML = request.text
+requestHTML = requests.get('https://www.nytimes.com').text
 
-soup = BeautifulSoup(requestHTML, 'html.parser')
-#title = soup.find('span', 'articletitle').string
+# Creating a parser
+parser = BeautifulSoup(requestHTML, 'lxml')
 
-# Print out a list of all the articles titles
-
-print(soup.prettify())
+for title in parser.find_all(class_="balancedHeadline"):
+    print(title.text.strip())
