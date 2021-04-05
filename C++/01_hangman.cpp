a4580c1bdbccaa483fbcf751061d00a38ca03e09
cynikkk <theo.abel53@gmail.com>
2021-04-05 22:02:42 +0200

    Added TODO + rename

---
+++ C++/01_hangman.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* TODO
+    * Générer mot caché via dictionnaire
+    * Initialiser vie (12 essais)
+    * Si lettre se trouve plusieurs fois afficher toutes les occurences
+    * Si lettre dans mot alors afficher lettre sinon vie--
+    * Afficher mot 
+    * Rendre lettres insensibles à la casse
+*/
+
+int main()
+{
+    
+    
+
+    return 0;
+}
