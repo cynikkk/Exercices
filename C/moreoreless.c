bfb604b977993266fc58c1c5c59e81f4d442bf47
cynikkk <theo.abel53@gmail.com>
2021-04-05 21:59:30 +0200

    finished program

--- C/moreoreless.c
+++ C/moreoreless.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-/* TODO
-    - Faire jeu de base (+ ou -)
-    - Faire le compteur de coups
-    - Demander si autre partie
-    - Créer plusieurs niveaux de difficulté (100, 1000, 10000)
-
-*/
 #define TRUE 1
 #define FALSE 0
+
+void clr() { system("clear"); }
 
 int main()
 {
@@ -17,20 +13,69 @@ int main()
     
     int input;
     int playing = TRUE;
-    int randomNumber = rand() % (100 + 1);
+    int difficulty = 0, tryNumber = 0, randomNumber;
 
-    do
+    while(playing)
     {
+        if (difficulty == 0) // Initialization
+        {
+            do
+            {
+                printf("Select difficulty :\n1. 0-100\n2. 0-1000\n3. 0-10000\n");
+                scanf("%d", &input);
+
+                if (input > 0 && input < 4)
+                {
+                    switch (input)
+                    {
+                    case 1:
+                        difficulty = 100;
+                        break;
+                    case 2:
+                        difficulty = 1000;
+                        break;
+                    case 3:
+                        difficulty = 10000;
+                        break;
+                    }
+                    clr();
+                    break; // Escape while
+                }
+                else
+                    printf("Cannot recognize difficulty, retry.");
+            } while(TRUE);
+
+            randomNumber = rand() % (difficulty + 1);
+        }
+        
         printf("What's the number ? : ");
         scanf("%d", &input);
 
         if (input == randomNumber)
-            printf("You won ! It was %d\n", randomNumber);
+        {
+            printf("You won ! It was %d\nYou found it in %d tries.\n", randomNumber, tryNumber);
+            printf("Do you want to replay ? (1 : yes, 0 : no) ");
+            scanf("%d", &input);
+
+            if (input)
+            {
+                playing = TRUE;
+                difficulty = 0;
+            }
+            else
+                playing = FALSE;
+        }
         else if (input < randomNumber)
+        {
             printf("More !\n");
+            tryNumber++;
+        }
         else
+        {
             printf("Less !\n");
-    } while (playing);
+            tryNumber++;
+        }
+    }
     
     return 0;
 }
