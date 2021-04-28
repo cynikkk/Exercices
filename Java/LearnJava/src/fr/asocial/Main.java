6f74170ae683d91f1ca8d9de1f0918e0a4b5068e
cynikkk <theo.abel53@gmail.com>
2021-04-28 13:55:25 +0200

    java update

---
+++ Java/LearnJava/src/fr/asocial/Main.java
@@ -0,0 +1,14 @@
+package fr.asocial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        say("Bonjour !\n");
+        say("Comment allez-vous ?");
+    }
+
+    public static void say(String message)
+    {
+        System.out.print(message);
+    }
+}
